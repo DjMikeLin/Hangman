fc5a08f28e94d98c8d1025fa0ea1d98c311f540b
djmikelin <linmichael97@yahoo.com>
2019-04-09 11:29:23 -0400

    1. Added images for the main game 2. Added StyleSheet directory and main.css 3. Added basic styles 4. Modified app.js to include image changes when user guesses wrong 5  Modified index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,38 @@
 class Game{
     constructor(){
-        //alphabet();
+        this.imagePaths = ['Images/Picture1.png', 'Images/Picture2.png', 'Images/Picture3.png', 
+        'Images/Picture4.png', 'Images/Picture5.png', 'Images/Picture6.png', 'Images/Picture7.png'];
+        this.changeImage(this.imagePaths[0]);
+        this.currLetter = '';
+        this.currWord = '';
+        this.guesses = 0;
     }
-
-    alphabet(){
-        for(let i = 65; i <= 90; i++){
-            let item = $('<li><div>'+ String.fromCharCode(i) +'</div></li>');
-            
-            item.click(() => {
-                console.log(item.text());
-                item.unbind("click");
-            });
     
-            $('ol').append(item); 
-        }
+    changeImage(path){
+        $('img').attr("src", path);
     }
 }
 
 $(() => {
     let currGame = new Game();
-    currGame.alphabet();
+    currGame.currWord = "Hello";
+    makeAlphabet(currGame);
 });
+
+function makeAlphabet(game){
+    for(let i = 65; i <= 90; i++){
+        let item = $('<li><div>'+ String.fromCharCode(i) +'</div></li>');
+        
+        item.click(() => {
+            item.unbind("click");
+            game.currLetter = item.text();
+
+            if(!game.currWord.includes(game.currLetter)){
+                game.changeImage(game.imagePaths[game.guesses + 1]);
+                game.guesses++;
+            }
+        });
+
+        $('ol').append(item); 
+    }
+}
