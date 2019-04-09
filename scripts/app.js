7cd08987f526d518804d26537fe92f8b5ee156fb
MikeLin <linmichael97@yahoo.com>
2019-04-09 07:16:35 -0400

    Updated app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,23 @@
 class Game{
     constructor(){
-
+        //alphabet();
     }
 
     alphabet(){
-        let obj = {};
         for(let i = 65; i <= 90; i++){
-            //let item = $('<li><div>'+ String.fromCharCode(i) +'</div></li>')
-            //obj[String.fromCharCode(i)] = 
+            let item = $('<li><div>'+ String.fromCharCode(i) +'</div></li>');
+            
+            item.click(() => {
+                console.log(item.text());
+                item.unbind("click");
+            });
+    
+            $('ol').append(item); 
         }
     }
 }
 
 $(() => {
-    for(let i = 65; i <= 90; i++){
-        let item = $('<li><div>'+ String.fromCharCode(i) +'</div></li>');
-        
-        item.click(() => {
-            console.log(item.text());
-            item.unbind("click");
-        });
-
-        $('ol').append(item); 
-    }
+    let currGame = new Game();
+    currGame.alphabet();
 });
