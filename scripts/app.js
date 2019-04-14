f277ceb84ee7d31f198f11bcce7f4e6ed4d737a4
MikeLin <linmichael97@yahoo.com>
2019-04-14 15:25:30 -0400

    Moved game class to another js file and linked it with index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,3 @@
-class Game{
-    constructor(){
-        this.imagePaths = ['Images/Picture1.png', 'Images/Picture2.png', 'Images/Picture3.png', 
-        'Images/Picture4.png', 'Images/Picture5.png', 'Images/Picture6.png', 'Images/Picture7.png'];
-        this.words = ['Joker', 'Us', 'Aquaman', 'Hellboy', 'After', 'Bumblebee', 'Aladdin',
-        'Venom', 'Hanna', 'Mirage', 'Split', 'Destroyer', 'It', 'Hereditary', 'Dumbo', 'Breakthrough',
-        'Penguins', 'Snatch', 'Gladiator', 'Crash', 'Transformers', 'Limitless', 'Goodfellas', 'Ghostbusters'];
-        this.changeImagePath = '';
-        this.currLetter = '';
-        this.currWord = this.words[Math.floor((Math.random() * this.words.length))].toUpperCase();
-        this.guesses = 0;
-        this.placeHolders = '';
-    }
-}
-
 $(() => {
     let currGame = new Game();
     changeImage(currGame.imagePaths[0]);
