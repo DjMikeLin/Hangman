50ebdcc159ddbefbf670e7187b37671c18895d3b
MikeLin <linmichael97@yahoo.com>
2019-04-14 14:35:13 -0400

    Added some more movie titles

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,8 @@ class Game{
         this.imagePaths = ['Images/Picture1.png', 'Images/Picture2.png', 'Images/Picture3.png', 
         'Images/Picture4.png', 'Images/Picture5.png', 'Images/Picture6.png', 'Images/Picture7.png'];
         this.words = ['Joker', 'Us', 'Aquaman', 'Hellboy', 'After', 'Bumblebee', 'Aladdin',
-        'Venom', 'Hanna', 'Mirage', 'Split', 'Destroyer', 'It', 'Hereditary'];
+        'Venom', 'Hanna', 'Mirage', 'Split', 'Destroyer', 'It', 'Hereditary', 'Dumbo', 'Breakthrough',
+        'Penguins', 'Snatch', 'Gladiator', 'Crash', 'Transformers', 'Limitless', 'Goodfellas', 'Ghostbusters'];
         this.changeImage(this.imagePaths[0]);
         this.currLetter = '';
         this.currWord = this.words[Math.floor((Math.random() * this.words.length))].toUpperCase();
